c8baa31cceb7627b2c9d20cc54a778730781c6f6
KlemenX <klemenmoderc@gmail.com>
2016-11-27 23:41:59 +0100

    oddaja strani  1. faza

--- README.md
+++
@@ -1,3 +0,0 @@
-O Projektu
-
-Izdelal bi spletno aplikacijo, kjer uporabnik vpiše podatke stanovanja(posamezno sobo in kvadraturo) ter program mu na podlagi tega izbere število panelov ter moč teh panelov in mu izdela ponudbo. Na izbiro bi imel tudi termostate in v ponudbo bi lahko vključil tudi te. Poskusil bi tudi izdelati tudi grafičen prikaz idealne postavitve panelov/montaže v posamezni sobi za stropno ali stensko montažo. Uporabnik bi se registriral v aplikacijo in lahko kasneje z prijavo urejal podatke. Ko pa se prijavi administrator se mu prikažejo vse dane ponudbe ki jih lahko tudi ureja, lahko dostopa do informacij kupcev, ter v primeru da se kupec odloči mu izdela tudi predračun.
--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-$(document).ready(function() {
-    $('.vstavi-sobo').on('submit', function(e) { //use on if jQuery 1.7+
- 		var vpisMerila = document.getElementById('testna_vrednost').value;
- 		var dolzinaDoc = document.getElementById("tloris").offsetWidth
- 		var dolzinaMerila = document.getElementById("resizable").offsetWidth
+document.addEventListener("DOMContentLoaded", function() {
+
+	document.getElementById("vstavi-sobo").addEventListener("submit", novaSoba);
+	document.getElementById("izracunBtn").addEventListener("click", izracunPanelov);
+	document.getElementById("toggle").addEventListener("click", toggleMenu);
+	document.getElementById("uplImg").addEventListener("change", naloziSliko);
+	window.addEventListener("resize", toggleOnResize, true);
+	document.getElementById("saveImgButton").addEventListener("click", shraniWebcam);
+	document.getElementById("camOnButton").addEventListener("click",odpriWebcam);
+
+	okrajsavaTexta();
+
+	$("#merilo")
+	.resizable({containment: 'parent',handles: "se",stop:resizeStop,resize: function(event, ui) {
+		        ui.size.height = ui.originalSize.height;
+		    }})
+	.draggable({containment: 'parent', stop:dragStop})
+
+
+	function okrajsavaTexta() {
+    	if(document.documentElement.clientWidth>600){
+			document.getElementById("st-sten-text").innerHTML = "Število zunanjih sten:";
+		}
+		else{
+			document.getElementById("st-sten-text").innerHTML = "Št. zunanjih sten:";
+		}
+	}
+
+	function novaSoba(e){
+ 		var stKvadratov=1;
+    	while(document.getElementById('kvadrat'+stKvadratov)){
+    		stKvadratov+=1;
+    	}
+ 		var vpisMerila = document.getElementById('meritev').value;
+ 		var dolzinaDoc = document.getElementById("tloris-div").offsetWidth
+ 		var dolzinaMerila = document.getElementById("merilo").offsetWidth
  		var pixNaMet =  dolzinaMerila/ vpisMerila;
-
- 		console.log(vpisMerila, dolzinaDoc,dolzinaMerila, pixNaMet);
-
  		var id = this.id;   	
         e.preventDefault();  //prevent form from submitting
         var data = $("#"+id+" :input").serializeArray();
@@ -14,73 +42,84 @@ $(document).ready(function() {
         console.log(data[0].value);
 		var wd = data[1].value;
 		var ht = data[2].value;	
+		var stene = data[3].value;		
+		var namembnost = data[4].value;	
 		var div=document.createElement("div");
-	    div.setAttribute("id","kvadrat"+id);
+	    div.setAttribute("id","kvadrat"+stKvadratov);
 	    div.setAttribute("class","kvadrat");
+	    div.setAttribute("imeSobe",data[0].value);
+	    div.setAttribute("visina",ht);
+	   	div.setAttribute("sirina",wd);
+	   	div.setAttribute("stevilo_sten",stene);
+	   	div.setAttribute("namembnost",namembnost);
+	   	div.style.position ="absolute";
 	    div.style.width =wd*pixNaMet+"px";
 	    div.style.height =ht*pixNaMet+"px";
+	    div.style.top =20+"px";
+		div.style.left =20+"px";
 	    div.innerHTML = div.innerHTML + data[0].value;
-	   	$('.palette').append(div);
-        $( ".kvadrat" ).draggable();
-	});
+	   	$('#tloris-div').append(div);
+	   	convert_to_percentageKvadrat($('#kvadrat'+stKvadratov));
+	   	document.getElementById('kvadrat'+stKvadratov).addEventListener('mousedown', getSobaData, false);
+	   	$('#kvadrat'+stKvadratov).draggable({containment: 'parent', stop:dragStopKvadrat});
+	}
 
-    $('#izracunBtn').on('click', function(e) { //use on if jQuery 1.7+
-    	var st = 1;
-    	var vpisMerila = document.getElementById('testna_vrednost').value;
- 		var dolzinaMerila = document.getElementById("resizable").offsetWidth
+	function getSobaData(el){
+	    document.getElementById('imeSobe').value = document.getElementById(this.id).getAttribute("imeSobe");
+	    document.getElementById('merax1').value = document.getElementById(this.id).getAttribute("sirina");
+	    document.getElementById('meray1').value = document.getElementById(this.id).getAttribute("visina");
+	    document.getElementById('ststen1').value = document.getElementById(this.id).getAttribute("stevilo_sten");
+	   	document.getElementById('namembnost1').value = document.getElementById(this.id).getAttribute("namembnost");
+	}
+
+  	function izracunPanelov(e){
+    	var vpisMerila = document.getElementById('meritev').value;
+ 		var dolzinaMerila = document.getElementById("merilo").offsetWidth
  		var pixNaMet =  dolzinaMerila/ vpisMerila;
- 		var wd = 1; var ht = 1;
+ 		var st = 1;
+ 		var wd = 0.8; var ht = 1.2;
     	while(document.getElementById('kvadrat'+st)){
 			var div=document.createElement("div");
 		    div.setAttribute("id","panel"+st);
 		    div.setAttribute("class","panel");
 		    div.style.width =wd*pixNaMet+"px";
 		    div.style.height =ht*pixNaMet+"px";
+		    div.style.top =($('#kvadrat'+st).height()- ht*pixNaMet)/2.0+"px";
+		    div.style.left =($('#kvadrat'+st).width()- wd*pixNaMet)/2.0+"px";		    
+		    div.style.position ="absolute";
 		    div.innerHTML = div.innerHTML + "700W";
-		   	document.getElementById('kvadrat'+st).append(div);
+		   	$('#kvadrat'+st).append(div);
+		   	convert_to_percentagePanel($('#panel'+st));
+		   	$('#panel'+st).draggable({containment: 'parent', stop:dragStopPanel});
 		   	st++;
-		$( ".panel" ).draggable();
     	}
-    });
+	}  
 
-    $('.potrdi-meritev').on('submit', function(e) { //use on if jQuery 1.7+
-		e.preventDefault();  //prevent form from submitting
-        var data1 = $("#vrednost :input").serializeArray();
-        //data1[0].value
-	});
+	function toggleMenu(e){
+		var elem = document.getElementById("sobeDiv");
+		console.log(parseInt(getComputedStyle(elem).getPropertyValue("margin-left")));
+		if (parseInt(getComputedStyle(elem).getPropertyValue("margin-left"))==0){
+			document.getElementById("sobeDiv").style.marginLeft=-160 +"px";
+		}
+		else{
+			document.getElementById("sobeDiv").style.marginLeft=0+"px";
+		}
+	}
 
-	$("#uplImg").change(function(e) {
-
-	    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+	function naloziSliko(e){
+	    for (var i = 0; i < e.srcElement.files.length; i++) {
 	        
-	        var file = e.originalEvent.srcElement.files[i];
-	        
-	        var img = document.createElement("img");
-	        img.setAttribute("id","tlorisSlika");
+	        var file = e.srcElement.files[i];
+
 	        var reader = new FileReader();
 	        reader.onloadend = function() {
-	             img.src = reader.result;
-	             document.getElementById('test10').src=reader.result;
-	             //document.getElementById('test9').style.backgroundImage=reader.result;	             
-	             console.log(reader.result);
+	             document.getElementById('tloris-hidden').src=reader.result;
+	             document.getElementById('tloris-div').style.backgroundImage='url('+reader.result+')';	             
+	             //console.log(reader.result);
 	        }
 	        reader.readAsDataURL(file);
-	        //console.log(readAsDataURL(file));
-	        img.style.maxWidth = "100%";
-	        img.style.maxHeight = "100%";
-	        $("#uplImg").after(img);
-
-	    }
-	});
-
-
-
-//
-	$('#element')
-	.resizable({containment: 'parent',handles: "se",stop:resizeStop,resize: function(event, ui) {
-		        ui.size.height = ui.originalSize.height;
-		    }})
-	.draggable({containment: 'parent', stop:dragStop})
+	    }		
+	}
 
 	function resizeStop(event, ui){
 	    convert_to_percentage($(this));
@@ -92,14 +131,136 @@ $(document).ready(function() {
 
 	function convert_to_percentage(el){
 	    var parent = el.parent();
-	    console.log(parseInt(el.css('left')),parent.width(),parseInt(el.css('left'))/parent.width()+8.0);
+	    console.log(parseInt(el.css('left')),parent.width());
 	    el.css({
 	        left:parseInt(el.css('left'))/(parent.width()-0.0)*100.0+"%",
 	        top: parseInt(el.css('top'))/(parent.height()-0.0)*100.0+"%",
-	        width: el.width()/(parent.width()-20.0)*100.0+"%",
-	        height: el.height()/(parent.height()+0.0)*100.0+"%"
+	        width: (el.width()+6)/(parent.width())*100.0+"%",
+	        height: (el.height())/(parent.height()+0.0)*100.0+"%"
 	    });
 	}
 
+	function dragStopKvadrat(event, ui){
+	    convert_to_percentageKvadrat($(this));
+	}
+	function dragStopPanel(event, ui){
+	    convert_to_percentagePanel($(this));
+	}
+	function convert_to_percentageKvadrat(el){
+	    var parent = el.parent();
+	    console.log(parseInt(el.css('left')),parent.width());
+	    el.css({
+	        left:parseInt(el.css('left'))/(parent.width()-0.0)*100.0+"%",
+	        top: (parseInt(el.css('top'))+1)/(parent.height()-0.0)*100.0+"%",
+	        width: (el.width()+4)/(parent.width())*100.0+"%",
+	        height: (el.height()+4)/(parent.height()+0.0)*100.0+"%"
+	    });
+	}
 
+	function convert_to_percentagePanel(el){
+	    var parent = el.parent();
+	    console.log(parseInt(el.css('left')),parent.width());
+	    el.css({
+	        left:parseInt(el.css('left'))/(parent.width()-0.0)*100.0+"%",
+	        top: (parseInt(el.css('top'))+1)/(parent.height()-0.0)*100.0+"%",
+	        width: (el.width()+4)/(parent.width())*100.0+"%",
+	        height: (el.height()+4)/(parent.height()+0.0)*100.0+"%"
+	    });
+	}
+
+	 function odpriWebcam(){
+
+	 	var vidObj = document.getElementById("videoEle");
+
+		errCallBack = function(error) {	// Video Error Handler 	
+		console.log("Video  error: ", error.code); 
+		};
+		
+		if(navigator.getUserMedia) { // Standard
+				navigator.getUserMedia({"video": true }, function(stream) {
+					vidObj.src = stream;
+					vidObj.play();
+				}, errCallBack);
+		} else if(navigator.webkitGetUserMedia) { // For  Chrome
+			/*
+			console.log("a");
+				document.getElementById("videoDiv").style.visibility="visible";
+				var video = document.querySelector('video');
+				navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+				navigator.oGetUserMedia;
+				navigator.getUserMedia({video:true},handleVideo, videoError);
+			*/
+			alert("Video zajem ni podprt v tem brskalniku.")
+
+		}
+		else if(navigator.mozGetUserMedia) { // For Firefox
+				console.log("b");
+				document.getElementById("videoDiv").style.visibility="visible";
+				navigator.mozGetUserMedia({ "video": true }, function(stream){
+				vidObj.src = window.URL.createObjectURL(stream);
+				vidObj.play();
+			}, errCallBack);
+		}
+		else{
+			alert("Video zajem ni podprt v tem brskalniku.")
+		}
+		function handleVideo(streamVid) {
+		    window.stream = streamVid;
+		    video.src = URL.createObjectURL(streamVid);
+		    video.play();		
+		}
+		function videoError(e) {
+		    document.write("can't use the webcam");
+		}
+
+	}
+
+
+	function toggleOnResize(){
+	    if (document.documentElement.clientWidth > 600) {
+			document.getElementById("sobeDiv").style.marginLeft=0+"px";
+			document.getElementById("toggleDivId").style.width=0+"px";
+			document.getElementById("st-sten-text").innerHTML = "Število zunanjih sten:";
+	    }
+	    else {
+			document.getElementById("sobeDiv").style.marginLeft=-160+"px";
+			document.getElementById("toggleDivId").style.width=3+"rem";		
+			document.getElementById("st-sten-text").innerHTML = "Št. zunanjih sten:";
+
+	    }
+	}
+
+	function shraniWebcam(){
+		var canvas = document.getElementById("c");
+		var video = document.getElementById("videoEle");
+		canvas.getContext("2d").drawImage(video, 0, 0, 1280, 960);
+		var img = canvas.toDataURL("image/png");
+		document.getElementById('tloris-hidden').src=img;
+        document.getElementById('tloris-div').style.backgroundImage='url('+img+')';	
+		document.getElementById("videoDiv").style.visibility="hidden";
+	}
+
+	/* -----FUNKCIJE SE ZA IMPLEMENTACIJO------
+    document.getElementById("obrni").addEventListener("click", function(e) {
+    	var stKvadratov=1;
+		var visina = document.getElementById('kvadrat1').getAttribute("visina");
+		var sirina = document.getElementById('kvadrat1').getAttribute("sirina");
+		document.getElementById('kvadrat1').style.width
+		document.getElementById('kvadrat1').setAttribute("visina",sirina);
+		document.getElementById('kvadrat1').setAttribute("sirina",visina);
+		var sir = document.getElementById('kvadrat1').style.width;
+		var vis = document.getElementById('kvadrat1').style.height;
+		console.log(vis);
+		console.log(sir);
+		document.getElementById('kvadrat1').style.width = vis;
+		document.getElementById('kvadrat1').style.height = sir	
+
+    });
+
+	document.getElementById("vpisMerila").addEventListener("submit", function(e) {
+			e.preventDefault();  //prevent form from submitting
+	        var data1 = $("#vrednost :input").serializeArray();
+	        //data1[0].value
+	});
+	*/
 });
